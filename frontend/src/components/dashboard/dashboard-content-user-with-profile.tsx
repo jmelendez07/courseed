ecbea1bfdfc514b3d15872391205f8a9ddec94b9
Jose <josemelendezramos123@gmail.com>
2025-04-23 18:55:44 -0500

    Commit rapido, pedro andante

---
+++ frontend/src/components/dashboard/dashboard-content-user-with-profile.tsx
@@ -0,0 +1,9 @@
+import { useAuth } from "@/providers/AuthProvider";
+
+function DashboardContentUserWithProfile() {
+    const authHook = useAuth();
+    
+    return ();
+}
+
+export default DashboardContentUserWithProfile;
